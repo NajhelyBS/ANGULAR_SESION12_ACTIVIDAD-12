94fd52fac5a8b84c7359bdd0848088b6becdf252
NajhelyBS <s5anbandasanchez@gmail.com>
2023-01-06 10:41:36 -0500

    Estilos creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ValidationStyleDirective } from './directive/validation-style.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValidationStyleDirective
   ],
   imports: [
     BrowserModule,
